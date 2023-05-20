3da419b2a18f59ef2c08ce2c4a171a1d4d8e363b
Victor Karpov <nilafe4@gmail.com>
2023-05-20 11:50:53 +0500

    Создан uunit test

--- functional_test.py
+++ functional_test.py
@@ -1,8 +1,22 @@
+import unittest
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
-driver.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    '''тест нового поситителя'''
 
-assert 'The install worked successfully! Congratulations!' in driver.title
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Тест: можно начать список и получить его позже"""
+        self.browser('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("Закончить тест!")
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
